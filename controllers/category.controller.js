8b520ee411f31da7312d2da7c150124f585f83b6
hunterocean1999 <lethanhtuanqn.1999@gmail.com>
2021-03-17 03:33:45 +0700

    no message

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -3,122 +3,127 @@ const Category = db.category;
 
 // Create and Save a new category
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.category_name) {
-        res.status(400).send({ message: "category_name can not be empty!" });
-        return;
-    }
+  // Validate request
+  if (!req.body.category_name) {
+    res.status(400).send({ message: "category_name can not be empty!" });
+    return;
+  }
 
-    // Create a category
-    const category = new Category({
-        category_name: req.body.category_name,
-        published: req.body.published ? req.body.published : false
+  // Create a category
+  const category = new Category({
+    category_name: req.body.category_name,
+    published: req.body.published ? req.body.published : false,
+  });
+
+  // Save Category in the database
+  category
+    .save(category)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Category.",
+      });
     });
-
-    // Save Category in the database
-    category
-        .save(category)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the Category."
-            });
-        });
 };
 
 // Retrieve all Category from the database.
 exports.getAll = (req, res) => {
-    const category_name = req.query.category_name;
-    var condition = category_name ? { category_name: { $regex: new RegExp(category_name), $options: "i" } } : {};
+  const category_name = req.query.category_name;
+  var condition = category_name
+    ? { category_name: { $regex: new RegExp(category_name), $options: "i" } }
+    : {};
 
-    Category.find(condition)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving category."
-            });
-        });
+  Category.find(condition)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving category.",
+      });
+    });
 };
 // Find a single Category with an id
 exports.getById = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Category.findById(id)
-        .then(data => {
-            if (!data)
-                res.status(404).send({ message: "Not found Category with id " + id });
-            else res.send(data);
-        })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: "Error retrieving Category with id=" + id });
-        });
+  Category.findById(id)
+    .then((data) => {
+      if (!data)
+        res.status(404).send({ message: "Not found Category with id " + id });
+      else res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Category with id=" + id });
+    });
 };
 
 // Update a Category by the id in the request
 exports.update = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
-            message: "Data to update can not be empty!"
-        });
-    }
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
 
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Category.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-        .then(data => {
-            if (!data) {
-                res.status(404).send({
-                    message: `Cannot update Category with id=${id}. Maybe Category was not found!`
-                });
-            } else res.send({ message: "Category was updated successfully." });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating Category with id=" + id
-            });
+  Category.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Category with id=${id}. Maybe Category was not found!`,
         });
+      } else res.send({ message: "Category was updated successfully." });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Category with id=" + id,
+      });
+    });
 };
 
 // Delete a Category with the specified id in the request
 exports.delete = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Category.findByIdAndRemove(id)
-        .then(data => {
-            if (!data) {
-                res.status(404).send({
-                    message: `Cannot delete Category with id=${id}. Maybe Category was not found!`
-                });
-            } else {
-                res.send({
-                    message: "Category was deleted successfully!"
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Could not delete Category with id=" + id
-            });
+  Category.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Category with id=${id}. Maybe Category was not found!`,
         });
+      } else {
+        res.send({
+          message: "Category was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Category with id=" + id,
+      });
+    });
 };
 
 // Delete all Category from the database.
 exports.deleteAll = (req, res) => {
-    Category.deleteMany({})
-        .then(data => {
-            res.send({
-                message: `${data.deletedCount} Category were deleted successfully!`
-            });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while removing all category."
-            });
-        });
+  Category.deleteMany({})
+    .then((data) => {
+      res.send({
+        message: `${data.deletedCount} Category were deleted successfully!`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all category.",
+      });
+    });
 };
--- routes/routes.news.js
+++ routes/routes.news.js
@@ -1,44 +1,40 @@
 "use strict";
 
-module.exports = function(app) {
-    var category = require("../controllers/category.controller");
-    var users = require("../controllers/users.controller");
-    var contact = require("../controllers/contact.controller");
-    var blog = require("../controllers/blog.controller");
-    var router = require("express").Router();
+module.exports = function (app) {
+  var category = require("../controllers/category.controller");
+  var users = require("../controllers/users.controller");
+  var contact = require("../controllers/contact.controller");
+  var blog = require("../controllers/blog.controller");
+  var router = require("express").Router();
 
+  // Đâu là router (đường dẫn),cái t chỉ là hàm lấy ở controller
+  router.post("/category/create", category.create);
+  router.get("/category/getAll", category.getAll);
 
-    router.post("/category/create", category.create);
+  router.get("/category/:id", category.getById);
 
-    router.get("/category/getAll", category.getAll);
+  router.put("/category/:id", category.update);
 
-    router.get("/category/:id", category.getById);
+  router["delete"]("/category/:id", category["delete"]);
 
-    router.put("/category/:id", category.update);
-
-    router["delete"]("/category/:id", category["delete"]);
-
-    router["delete"]("/category/", category.deleteAll);
-    // ----------------------------------//
-    router.post("/users/create", users.create);
-    router.get("/users/getAll", users.getAll);
-    router.get("/users/:id", users.getById);
-    router.put("/users/:id", users.update);
-    router.delete("/users/:id", users.delete);
-    // --------------------------------------//
-    router.post("/contact/create", contact.create);
-    router.get("/contact/getAll", contact.getAll);
-    router.get("/contact/:id", contact.getById);
-    router.put("/contact/:id", contact.update);
-    router.delete("/contact/:id", contact.delete);
-    // ---------------------------------------------
-    router.post("/blog/create", blog.create);
-    router.get("/blog/getAll", blog.getAll);
-    router.get("/blog/:id", blog.getById);
-    router.put("/blog/:id", blog.update);
-    router.delete("/blog/:id", blog.delete);
-    app.use('/api', router);
-
-
-
+  router["delete"]("/category/", category.deleteAll);
+  // ----------------------------------//
+  router.post("/users/create", users.create);
+  router.get("/users/getAll", users.getAll);
+  router.get("/users/:id", users.getById);
+  router.put("/users/:id", users.update);
+  router.delete("/users/:id", users.delete);
+  // --------------------------------------//
+  router.post("/contact/create", contact.create);
+  router.get("/contact/getAll", contact.getAll);
+  router.get("/contact/:id", contact.getById);
+  router.put("/contact/:id", contact.update);
+  router.delete("/contact/:id", contact.delete);
+  // ---------------------------------------------
+  router.post("/blog/create", blog.create);
+  router.get("/blog/getAll", blog.getAll);
+  router.get("/blog/:id", blog.getById);
+  router.put("/blog/:id", blog.update);
+  router.delete("/blog/:id", blog.delete);
+  app.use("/api", router);
 };
